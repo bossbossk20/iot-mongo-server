cbb34447be81373cc7e3e3e4b455f47c35a85118
thanawat <tha_chookes@hotmail.com>
2015-10-28 00:45:47 +0700

    backend

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost')
-
 var app = express()
 	
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+var iot = require('./models/iot/iot.route.js')
+app.use('/api/iot', iot)
 
-	app.get('/' , function(req,res){
-		res.send("hello")
-	})
+	// app.get('/' , function(req,res){
+	// 	res.send("hello")
+	// })
 	
 	
 	
---
+++ models/iot/iot.schema.js
@@ -0,0 +1,20 @@
+;(function () {
+  'use strict'
+  var modelName = 'iot'
+  var mongoose = require('mongoose')
+  var Schema = mongoose.Schema
+
+  var schema = new Schema({
+    data: {
+      timestamp: Number,
+      iot_id: Number,
+      temperature: Number,
+      relative_humidity: Number
+    }
+
+    // date: { type: Date, default: Date.now }
+
+  })
+
+  module.exports = mongoose.model(modelName, schema)
+})()
